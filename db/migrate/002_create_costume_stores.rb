46b0f660fe7af0d31c808e57c1ef9cdbf1714f1b
Harley McDowell <hmcdowell95@gmail.com>
2020-04-05 05:23:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,8 @@
 # Create your costume_stores migration here
 class CostumeStores < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :costume_stores do |x|
+      x.string :name 
+      x.string :location 
+      x.
