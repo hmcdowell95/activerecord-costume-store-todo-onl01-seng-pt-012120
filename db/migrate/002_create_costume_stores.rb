93a4a06b7c2cda608d409c4c081e080e594d94e2
Harley McDowell <hmcdowell95@gmail.com>
2020-04-05 06:11:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       x.integer :costume_inventory
       x.integer :num_of_employees
       x.boolean :still_in_business
-      x.string :opening_time
-      x.string :closing_time
+      x.datetime :opening_time
+      x.datetime :closing_time
     end
   end
 end
