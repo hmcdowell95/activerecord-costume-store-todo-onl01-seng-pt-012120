f1442ddce3af880d56a76753fefaf56a2ac2fee3
Harley McDowell <hmcdowell95@gmail.com>
2020-04-05 06:09:57 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,5 +10,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       x.boolean :family_friendly
       x.string :opening_date
       x.string :closing_date
-      
+      x.text :description
+    end
+  end 
+end 
       
